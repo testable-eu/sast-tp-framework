06593c544598f67d75623fb750b3f42f18671f3a
felix-20 <felix-20@users.noreply.github.com>
2023-06-07 15:34:58 +0200

    CodeQL in multiple versions

--- SAST/codeql/codeql-install.sh
+++ SAST/codeql/codeql-install.sh
@@ -1,14 +1,45 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 VERSION_LIST_FILE=$1
+CODEQL_DIR=/codeql
 
-mkdir ./codeql
+mkdir $CODEQL_DIR
 
-yq eval -M '{.versions}' $VERSION_LIST_FILE  | xargs -n2 | while read NAME LINK; do
+# Make sure the file contains a newline at the end
+echo -en '\n' >> "$VERSION_LIST_FILE"
 
-    wget $LINK
-    tar -xvzf ./codeql-bundle-linux64.tar.gz -C ./codeql && rm ./codeql-bundle-linux64.tar.gz
-    mv ./codeql/codeql ./codeql/$NAME
+name=""
+link=""
+# Read the VERSION_LIST_FILE line by line
+while IFS=: read -r key value; do
+  # Remove leading/trailing spaces from the key and value (remove " as well)
+  key=$(echo "$key" | xargs)
+  value=$(echo "$value" | xargs | tr -d '\"')
 
-    ./codeql/$NAME/codeql resolve languages
-    ./codeql/$NAME/codeql resolve qlpacks
-done
+  # Check if the line contains "link" or "name" field
+  if [[ "$key" == "link" ]]; then
+    link=$value
+  elif [[ "$key" == "name" ]]; then
+    name=$value
+  fi
+
+  # Check if both name and link are set
+  if [[ -n "$name" && -n "$link" ]]; then
+    # Download the codeql version using wget, extract it and move it to the codeql directory
+    echo "Downloading $name from $link"
+    wget -q "$link"
+    tar -xzf ./codeql-bundle-linux64.tar.gz -C "$CODEQL_DIR"
+    mv "$CODEQL_DIR/codeql" "$CODEQL_DIR/$name"
+
+    # setup codeql version
+    "$CODEQL_DIR/$name/codeql" resolve languages
+    "$CODEQL_DIR/$name/codeql" resolve qlpacks
+
+    # remove the downloaded .tar.gz
+    rm ./codeql-bundle-linux64.tar.gz
+
+    # Reset the variables for the next entry
+    name=""
+    link=""
+  fi
+done < "$VERSION_LIST_FILE"
