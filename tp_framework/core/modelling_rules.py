1c8c4c805de7a928eda0f739370408056aa88732
I745280 <sachiththa.konara.mudiyanselage@sap.com>
2024-02-22 11:41:48 +0100

    Refactor sast tools imports in modeling rules

--- tp_framework/core/modelling_rules.py
+++ tp_framework/core/modelling_rules.py
@@ -4,17 +4,18 @@ from typing import Dict
 import core.utils
 from core import utils
 
-from sast.tools import tools as sast_tools
+import sast.sast_tools as sast_tools
+
 
 async def scan(src_dir: Path, tools: list[Dict], language: str, modelling_rules: Path = None):
     if not src_dir.is_dir():
         raise
     if not tools:
         raise
-    
+
     results = []
     for tool in tools:
-        sast = sast_tools.get((tool["name"], tool["version"]))
+        sast = sast_tools.get_sast_tool(tool["name"], tool["version"])
         res = await sast.launcher(src_dir, language, output_dir, use_mvn=(src_dir / "pom.xml").exists(),
                                   apply_remediation=True, modelling_rules=modelling_rules)
 
