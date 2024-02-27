cbb40e54a6d47600ab54eef329dd57ea53ed56e0
Sachiththa Bandaranayake <sacume3@gmail.com>
2024-02-27 11:39:38 +0100

    Uncomment tests

--- qualitytests/cli/test_interface.py
+++ qualitytests/cli/test_interface.py
@@ -129,169 +129,169 @@ class TestInterface:
         assert logfile and logfile.is_file()
 
 
-    # @pytest.mark.asyncio
-    # async def test_sast_measurement(self, tmp_path, capsys, mocker):
-    #     init = {}
-    #     init_measure_test(init, mocker)
-    #     await interface.measure_list_patterns(init["patterns"], init["language"], tools=init["tools"],
-    #                                     tp_lib_path=init["tp_lib_path"], output_dir=tmp_path, workers=2)
-    #     out = capsys.readouterr().out
-    #     captured_out_lines = out.split("\n")
-    #     sys.stdout.write(out)
-    #     output_dir = get_result_output_dir(captured_out_lines)
-    #     assert (output_dir and output_dir.iterdir())
-    #     logfile = get_logfile_path(captured_out_lines)
-    #     assert logfile and logfile.is_file()
-    #
-    #
-    # def test_sast_report_1(self, tmp_path, capsys, mocker):
-    #     init = {}
-    #     init_sastreport_test(init, mocker)
-    #     # Test 1: it does not consider the following params
-    #     # - export_file: Path = None,
-    #     # - output_dir: Path = Path(config.RESULT_DIR).resolve(),
-    #     # - only_last_measurement: bool = True):
-    #     interface.report_sast_measurement_for_pattern_list(
-    #         init["tools"], init["language"], init["patterns"], init["tp_lib_path"]
-    #     )
-    #     out = capsys.readouterr().out
-    #     captured_out_lines = out.split("\n")
-    #     sys.stdout.write(out)
-    #     output_dir = get_result_output_dir(captured_out_lines)
-    #     assert (output_dir and output_dir.iterdir())
-    #     logfile = get_logfile_path(captured_out_lines)
-    #     assert logfile and logfile.is_file()
-    #
-    #
-    # def test_sast_report_2(self, tmp_path, capsys, mocker):
-    #     init = {}
-    #     init_sastreport_test(init, mocker)
-    #     export_file = "test_export.csv"
-    #     # Test 2: it does not consider the following params
-    #     # - only_last_measurement: bool = True):
-    #     interface.report_sast_measurement_for_pattern_list(
-    #         init["tools"], init["language"], init["patterns"], init["tp_lib_path"],
-    #         export_file=export_file, output_dir=tmp_path
-    #     )
-    #     out = capsys.readouterr().out
-    #     captured_out_lines = out.split("\n")
-    #     sys.stdout.write(out)
-    #     output_dir = get_result_output_dir(captured_out_lines)
-    #     assert (output_dir and output_dir.iterdir())
-    #     logfile = get_logfile_path(captured_out_lines)
-    #     assert logfile and logfile.is_file()
-    #     assert (output_dir / export_file).is_file()
-    #
-    #
-    # def test_check_discovery_rules_1(self, tmp_path, capsys, mocker):
-    #     init = {}
-    #     self._init_discovery_test(tmp_path, mocker)
-    #     init_test(init)
-    #     export_file = "test_export.csv"
-    #     interface.check_discovery_rules(
-    #         init["language"], init["patterns"], 0,
-    #         tp_lib_path=init["tp_lib_path"], export_file=export_file
-    #     )
-    #     out = capsys.readouterr().out
-    #     captured_out_lines = out.split("\n")
-    #     sys.stdout.write(out)
-    #     output_dir = get_result_output_dir(captured_out_lines)
-    #     assert (output_dir and output_dir.iterdir())
-    #     logfile = get_logfile_path(captured_out_lines)
-    #     assert logfile and logfile.is_file()
-    #
-    #
-    # def test_check_discovery_rules_2(self, tmp_path, capsys, mocker):
-    #     init = {}
-    #     self._init_discovery_test(tmp_path, mocker)
-    #     init_test(init)
-    #     export_file = "test_export.csv"
-    #     interface.check_discovery_rules(
-    #         init["language"], init["patterns"], 0,
-    #         tp_lib_path=init["tp_lib_path"], export_file=export_file, output_dir=tmp_path
-    #     )
-    #     out = capsys.readouterr().out
-    #     captured_out_lines = out.split("\n")
-    #     sys.stdout.write(out)
-    #     output_dir = get_result_output_dir(captured_out_lines)
-    #     assert (output_dir and output_dir.iterdir())
-    #     logfile = get_logfile_path(captured_out_lines)
-    #     assert logfile and logfile.is_file()
-    #
-    #
-    # def init_check_discovery_rules_3(self, init, tmp_path, mocker):
-    #     init["language"] = "PHP"
-    #     init["patterns"] = [32, 37]
-    #     init["tp_lib_path"] = join_resources_path("sample_patlib_issue9")
-    #     mocker.patch("core.discovery.run_generate_cpg_cmd",
-    #                  return_value="Done",
-    #                  side_effect=create_mock_cpg(tmp_path / "cpg_binary.bin"))
-    #     mocker.patch("core.discovery.run_joern_scala_query_for_test",
-    #                  return_value="Done")
-    #     exp_discovery_rule_results = (
-    #         str(join_resources_path("sample_joern") / "binary.bin"),
-    #         "1_static_variables_iall",
-    #         1
-    #     )
-    #     mocker.patch("core.discovery.run_and_process_discovery_rule",
-    #                  return_value=bytes(str(exp_discovery_rule_results), 'utf-8'))
-    #
-    #
-    # def test_check_discovery_rules_3(self, tmp_path, capsys, mocker):
-    #     init = {}
-    #     self.init_check_discovery_rules_3(init, tmp_path, mocker)
-    #     export_file = "test_export.csv"
-    #     interface.check_discovery_rules(
-    #         init["language"], init["patterns"], 0,
-    #         tp_lib_path=init["tp_lib_path"], export_file=export_file, output_dir=tmp_path
-    #     )
-    #     out = capsys.readouterr().out
-    #     captured_out_lines = out.split("\n")
-    #     sys.stdout.write(out)
-    #     output_dir = get_result_output_dir(captured_out_lines)
-    #     assert (output_dir and output_dir.iterdir())
-    #     logfile = get_logfile_path(captured_out_lines)
-    #     assert logfile and logfile.is_file()
-    #
-    #
-    # def test_repair_patterns_not_including_readme(self):
-    #     sample_tp_lib = join_resources_path("sample_patlib")
-    #     test_pattern = create_pattern()
-    #     with patch("core.pattern.Pattern.init_from_id_and_language") as init_pattern_mock, \
-    #         patch("core.pattern.Pattern.repair") as patternrepair_mock, \
-    #         patch("core.utils.check_file_exist") as check_file_exists_mock, \
-    #         patch("core.utils.check_measurement_results_exist") as measurement_result_exist_mock, \
-    #         patch("pathlib.Path.mkdir") as mkdir_mock:
-    #         init_pattern_mock.return_value = test_pattern
-    #         interface.repair_patterns("JS", [1,2,3], None, True, Path("measurements"), Path("dr_results.csv"), Path("out"), sample_tp_lib)
-    #
-    #     patternrepair_mock.assert_called_with(False,
-    #                                           discovery_rule_results=Path("dr_results.csv"),
-    #                                           measurement_results=Path("measurements"),
-    #                                           masking_file=None)
-    #     expected_calls = [call(1, "JS", sample_tp_lib), call(2, "JS", sample_tp_lib), call(3, "JS", sample_tp_lib)]
-    #     init_pattern_mock.assert_has_calls(expected_calls)
-    #     check_file_exists_mock.assert_not_called()
-    #     measurement_result_exist_mock.assert_not_called()
-    #     mkdir_mock.assert_called()
-    #
-    # def test_repair_patterns_not_including_readme(self):
-    #     sample_tp_lib = join_resources_path("sample_patlib")
-    #     test_pattern = create_pattern()
-    #     with patch("core.pattern.Pattern.init_from_id_and_language") as init_pattern_mock, \
-    #         patch("core.pattern.Pattern.repair") as patternrepair_mock, \
-    #         patch("core.utils.check_file_exist") as check_file_exists_mock, \
-    #         patch("core.utils.check_measurement_results_exist") as measurement_result_exist_mock, \
-    #         patch("pathlib.Path.mkdir") as mkdir_mock:
-    #         init_pattern_mock.return_value = test_pattern
-    #         interface.repair_patterns("JS", [1,2,3], None, False, Path("measurements"), Path("dr_results.csv"), Path("out"), sample_tp_lib)
-    #
-    #     patternrepair_mock.assert_called_with(True,
-    #                                           discovery_rule_results=Path("dr_results.csv"),
-    #                                           measurement_results=Path("measurements"),
-    #                                           masking_file=None)
-    #     expected_calls = [call(1, "JS", sample_tp_lib), call(2, "JS", sample_tp_lib), call(3, "JS", sample_tp_lib)]
-    #     init_pattern_mock.assert_has_calls(expected_calls)
-    #     check_file_exists_mock.assert_called()
-    #     measurement_result_exist_mock.assert_called_once()
-    #     mkdir_mock.assert_called()
+    @pytest.mark.asyncio
+    async def test_sast_measurement(self, tmp_path, capsys, mocker):
+        init = {}
+        init_measure_test(init, mocker)
+        await interface.measure_list_patterns(init["patterns"], init["language"], tools=init["tools"],
+                                        tp_lib_path=init["tp_lib_path"], output_dir=tmp_path, workers=2)
+        out = capsys.readouterr().out
+        captured_out_lines = out.split("\n")
+        sys.stdout.write(out)
+        output_dir = get_result_output_dir(captured_out_lines)
+        assert (output_dir and output_dir.iterdir())
+        logfile = get_logfile_path(captured_out_lines)
+        assert logfile and logfile.is_file()
+
+
+    def test_sast_report_1(self, tmp_path, capsys, mocker):
+        init = {}
+        init_sastreport_test(init, mocker)
+        # Test 1: it does not consider the following params
+        # - export_file: Path = None,
+        # - output_dir: Path = Path(config.RESULT_DIR).resolve(),
+        # - only_last_measurement: bool = True):
+        interface.report_sast_measurement_for_pattern_list(
+            init["tools"], init["language"], init["patterns"], init["tp_lib_path"]
+        )
+        out = capsys.readouterr().out
+        captured_out_lines = out.split("\n")
+        sys.stdout.write(out)
+        output_dir = get_result_output_dir(captured_out_lines)
+        assert (output_dir and output_dir.iterdir())
+        logfile = get_logfile_path(captured_out_lines)
+        assert logfile and logfile.is_file()
+
+
+    def test_sast_report_2(self, tmp_path, capsys, mocker):
+        init = {}
+        init_sastreport_test(init, mocker)
+        export_file = "test_export.csv"
+        # Test 2: it does not consider the following params
+        # - only_last_measurement: bool = True):
+        interface.report_sast_measurement_for_pattern_list(
+            init["tools"], init["language"], init["patterns"], init["tp_lib_path"],
+            export_file=export_file, output_dir=tmp_path
+        )
+        out = capsys.readouterr().out
+        captured_out_lines = out.split("\n")
+        sys.stdout.write(out)
+        output_dir = get_result_output_dir(captured_out_lines)
+        assert (output_dir and output_dir.iterdir())
+        logfile = get_logfile_path(captured_out_lines)
+        assert logfile and logfile.is_file()
+        assert (output_dir / export_file).is_file()
+
+
+    def test_check_discovery_rules_1(self, tmp_path, capsys, mocker):
+        init = {}
+        self._init_discovery_test(tmp_path, mocker)
+        init_test(init)
+        export_file = "test_export.csv"
+        interface.check_discovery_rules(
+            init["language"], init["patterns"], 0,
+            tp_lib_path=init["tp_lib_path"], export_file=export_file
+        )
+        out = capsys.readouterr().out
+        captured_out_lines = out.split("\n")
+        sys.stdout.write(out)
+        output_dir = get_result_output_dir(captured_out_lines)
+        assert (output_dir and output_dir.iterdir())
+        logfile = get_logfile_path(captured_out_lines)
+        assert logfile and logfile.is_file()
+
+
+    def test_check_discovery_rules_2(self, tmp_path, capsys, mocker):
+        init = {}
+        self._init_discovery_test(tmp_path, mocker)
+        init_test(init)
+        export_file = "test_export.csv"
+        interface.check_discovery_rules(
+            init["language"], init["patterns"], 0,
+            tp_lib_path=init["tp_lib_path"], export_file=export_file, output_dir=tmp_path
+        )
+        out = capsys.readouterr().out
+        captured_out_lines = out.split("\n")
+        sys.stdout.write(out)
+        output_dir = get_result_output_dir(captured_out_lines)
+        assert (output_dir and output_dir.iterdir())
+        logfile = get_logfile_path(captured_out_lines)
+        assert logfile and logfile.is_file()
+
+
+    def init_check_discovery_rules_3(self, init, tmp_path, mocker):
+        init["language"] = "PHP"
+        init["patterns"] = [32, 37]
+        init["tp_lib_path"] = join_resources_path("sample_patlib_issue9")
+        mocker.patch("core.discovery.run_generate_cpg_cmd",
+                     return_value="Done",
+                     side_effect=create_mock_cpg(tmp_path / "cpg_binary.bin"))
+        mocker.patch("core.discovery.run_joern_scala_query_for_test",
+                     return_value="Done")
+        exp_discovery_rule_results = (
+            str(join_resources_path("sample_joern") / "binary.bin"),
+            "1_static_variables_iall",
+            1
+        )
+        mocker.patch("core.discovery.run_and_process_discovery_rule",
+                     return_value=bytes(str(exp_discovery_rule_results), 'utf-8'))
+
+
+    def test_check_discovery_rules_3(self, tmp_path, capsys, mocker):
+        init = {}
+        self.init_check_discovery_rules_3(init, tmp_path, mocker)
+        export_file = "test_export.csv"
+        interface.check_discovery_rules(
+            init["language"], init["patterns"], 0,
+            tp_lib_path=init["tp_lib_path"], export_file=export_file, output_dir=tmp_path
+        )
+        out = capsys.readouterr().out
+        captured_out_lines = out.split("\n")
+        sys.stdout.write(out)
+        output_dir = get_result_output_dir(captured_out_lines)
+        assert (output_dir and output_dir.iterdir())
+        logfile = get_logfile_path(captured_out_lines)
+        assert logfile and logfile.is_file()
+
+
+    def test_repair_patterns_not_including_readme(self):
+        sample_tp_lib = join_resources_path("sample_patlib")
+        test_pattern = create_pattern()
+        with patch("core.pattern.Pattern.init_from_id_and_language") as init_pattern_mock, \
+            patch("core.pattern.Pattern.repair") as patternrepair_mock, \
+            patch("core.utils.check_file_exist") as check_file_exists_mock, \
+            patch("core.utils.check_measurement_results_exist") as measurement_result_exist_mock, \
+            patch("pathlib.Path.mkdir") as mkdir_mock:
+            init_pattern_mock.return_value = test_pattern
+            interface.repair_patterns("JS", [1,2,3], None, True, Path("measurements"), Path("dr_results.csv"), Path("out"), sample_tp_lib)
+
+        patternrepair_mock.assert_called_with(False,
+                                              discovery_rule_results=Path("dr_results.csv"),
+                                              measurement_results=Path("measurements"),
+                                              masking_file=None)
+        expected_calls = [call(1, "JS", sample_tp_lib), call(2, "JS", sample_tp_lib), call(3, "JS", sample_tp_lib)]
+        init_pattern_mock.assert_has_calls(expected_calls)
+        check_file_exists_mock.assert_not_called()
+        measurement_result_exist_mock.assert_not_called()
+        mkdir_mock.assert_called()
+
+    def test_repair_patterns_not_including_readme(self):
+        sample_tp_lib = join_resources_path("sample_patlib")
+        test_pattern = create_pattern()
+        with patch("core.pattern.Pattern.init_from_id_and_language") as init_pattern_mock, \
+            patch("core.pattern.Pattern.repair") as patternrepair_mock, \
+            patch("core.utils.check_file_exist") as check_file_exists_mock, \
+            patch("core.utils.check_measurement_results_exist") as measurement_result_exist_mock, \
+            patch("pathlib.Path.mkdir") as mkdir_mock:
+            init_pattern_mock.return_value = test_pattern
+            interface.repair_patterns("JS", [1,2,3], None, False, Path("measurements"), Path("dr_results.csv"), Path("out"), sample_tp_lib)
+
+        patternrepair_mock.assert_called_with(True,
+                                              discovery_rule_results=Path("dr_results.csv"),
+                                              measurement_results=Path("measurements"),
+                                              masking_file=None)
+        expected_calls = [call(1, "JS", sample_tp_lib), call(2, "JS", sample_tp_lib), call(3, "JS", sample_tp_lib)]
+        init_pattern_mock.assert_has_calls(expected_calls)
+        check_file_exists_mock.assert_called()
+        measurement_result_exist_mock.assert_called_once()
+        mkdir_mock.assert_called()
